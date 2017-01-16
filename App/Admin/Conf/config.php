0abbcd3a8031d375ef7f07470b46650b13420fa5
journey <admin@libaoka.com>
2017-01-16 09:50:28 +0800

    修改独立分组菜单链接地址模式，当前默认开放2种访问方式，分组模式、子域名绑定模式。通过Admin分组中的SUB_DOMAIN_MODE控制是否追加分组地址

--- App/Admin/Conf/config.php
+++ App/Admin/Conf/config.php
@@ -5,4 +5,5 @@ return array(
 	'LOG_DB_TAGS'			=>  8,   // 日志记录数据库标记长度 
 	'LOG_FILE_SIZE' 		=>  40960,   // 日志文件大小限制
 	'LOG_DB_FIELDS'			=>  "logtags,logtext",   // 日志记录数据库字段 0为tags 1为内容
+	"SUB_DOMAIN_MODE"		=> false,//是否开启子域名模式
 );
